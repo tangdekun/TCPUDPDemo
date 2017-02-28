b0b61f3549363794f225c6c7a620cb7eeb2b802c
John <994601812@qq.com>
2017-02-28 23:04:08 +0800

    修改IP

--- app/src/main/java/com/view/john/tcpudpdemo/Constant.java
+++ app/src/main/java/com/view/john/tcpudpdemo/Constant.java
@@ -2,7 +2,7 @@ package com.view.john.tcpudpdemo;
 
 public class Constant {
     public static final int PORT = 8088;
-    public static final String TARGET_IP = "192.168.253.5";
+    public static final String TARGET_IP = "192.168.253.7";
 
 
 }
