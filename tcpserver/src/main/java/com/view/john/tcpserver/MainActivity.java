58ede43cac13760a74d20b4716bd538945826dc1
John <994601812@qq.com>
2017-03-03 00:36:03 +0800

    本机自建服务器并完成通信

--- tcpserver/src/main/java/com/view/john/tcpserver/MainActivity.java
+++ tcpserver/src/main/java/com/view/john/tcpserver/MainActivity.java
@@ -1,18 +1,19 @@
 package com.view.john.tcpserver;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -26,7 +27,7 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String TAG = "TcpRunnable";
+    public static final String TAG = "TcpRunnable";
     private TextView showTv;
     private EditText msgEt;
     private static ServerSocket mServerSocket = null;
@@ -35,6 +36,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static BufferedWriter mBufferedWriter = null;
     private static String msg;
     private static Handler myHandler;
+    private static final int MESSAGE_RECEIVER_NEW_MSG = 1;
+    private static final int MESSAGE_SOCKET_CONNECTED = 2;
+    private static boolean IS_CONNENTED = false;
+    Intent serverintent = null;
 
 
     @Override
@@ -43,18 +48,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         showTv = (TextView) findViewById(R.id.showMessage);
         msgEt = (EditText) findViewById(R.id.message_et);
-        EventBus.getDefault().register(this);
         myHandler = new MyHandler(MainActivity.this);
+        serverintent = new Intent(MainActivity.this, TcpServer.class);
+
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.startServer:
-                StartServerRunnable mStartServerRunnable = new StartServerRunnable();
+               startService(serverintent);
+                IS_CONNENTED = true;
+                StartSocketRunnable mStartServerRunnable = new StartSocketRunnable();
                 new Thread(mStartServerRunnable).start();
                 break;
             case R.id.closeServer:
+               stopService(serverintent);
                 try {
                     closeSocket();
                 } catch (IOException e) {
@@ -63,6 +73,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.sendMsg:
                 msg = msgEt.getText().toString();
+                if (msg != null) {
+                    StringBuilder sb = new StringBuilder(showTv.getText() + "\n");
+                    sb.append("client:\n" + msg + "\n");
+                    showTv.setText(sb.toString());
+                    try {
+                        mBufferedWriter = new BufferedWriter(new OutputStreamWriter(mSocket.getOutputStream(), "utf-8"));
+                        mBufferedWriter.write(msg + "\n");
+                        mBufferedWriter.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
                 break;
 
             default:
@@ -72,17 +94,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void showMessage(MessageMsg messageMsg) {
-        StringBuilder sb = new StringBuilder(showTv.getText().toString() + "\n");
-        sb.append(messageMsg + "\n");
-        showTv.setText(sb.toString());
-    }
+//    @Subscribe(threadMode = ThreadMode.MAIN)
+//    public void showMessage(MessageMsg messageMsg) {
+//        StringBuilder sb = new StringBuilder(showTv.getText().toString() + "\n");
+//        sb.append(messageMsg + "\n");
+//        showTv.setText(sb.toString());
+//    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        EventBus.getDefault().unregister(this);
+//        EventBus.getDefault().unregister(this);
         try {
             closeSocket();
         } catch (IOException e) {
@@ -91,114 +113,189 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    private static class ReceiverRunnable implements Runnable {
+//    private static class ReceiverRunnable implements Runnable {
+//        @Override
+//        public void run() {
+//            while (true) {
+//                try {
+//                    Log.d(TAG, "receiver thread open");
+//                    mBufferedReader = new BufferedReader(new InputStreamReader(mSocket.getInputStream(), "utf-8"));
+//                    StringBuilder sb = new StringBuilder();
+//                    String data = null;
+//                    while ((data = mBufferedReader.readLine()) != null) {
+//                        sb.append(data);
+//                    }
+//                    EventBus.getDefault().post(new MessageMsg(sb.toString()));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+
+//    private static class SendRunnable implements Runnable {
+//
+//        @Override
+//        public void run() {
+//            while (true) {
+//                try {
+//                    Log.d(TAG, "send thread open");
+//                    if (msg != null) {
+//                        Log.d(TAG, msg);
+//                        mBufferedWriter = new BufferedWriter(new OutputStreamWriter(mSocket.getOutputStream(), "utf-8"));
+//                        mBufferedWriter.write(msg + "\n");
+//                        mBufferedWriter.flush();
+//                        msg = null;
+//                    }
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+
+//    private static class StartServerRunnable implements Runnable {
+//
+//        @Override
+//        public void run() {
+//            try {
+//                if (mServerSocket != null) {
+//                    mServerSocket = new ServerSocket(Constant.PORT);
+//                    myHandler.sendEmptyMessage(1);
+//                    while (true) {
+//                        mSocket = mServerSocket.accept();
+//                        if (mSocket != null) {
+//                            myHandler.sendEmptyMessage(2);
+//                        }
+//                    }
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+
+    private class StartSocketRunnable implements Runnable {
         @Override
         public void run() {
-            while (true) {
+            Socket socket = null;
+            while (socket == null) {
                 try {
-                    Log.d(TAG, "receiver thread open");
-                    mBufferedReader = new BufferedReader(new InputStreamReader(mSocket.getInputStream(), "utf-8"));
-                    StringBuilder sb = new StringBuilder();
-                    String data = null;
-                    while ((data = mBufferedReader.readLine()) != null) {
-                        sb.append(data);
-                    }
-                    EventBus.getDefault().post(new MessageMsg(sb.toString()));
+                    socket = new Socket(getLocal(), Constant.PORT);
+                    mSocket = socket;
+                    mBufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    myHandler.sendEmptyMessage(MESSAGE_RECEIVER_NEW_MSG);
                 } catch (IOException e) {
+                    SystemClock.sleep(1000);
                     e.printStackTrace();
                 }
             }
-        }
-    }
-
-    private static class SendRunnable implements Runnable {
-
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    Log.d(TAG, "send thread open");
-                    if (msg != null) {
-                        Log.d(TAG, msg);
-                        mBufferedWriter = new BufferedWriter(new OutputStreamWriter(mSocket.getOutputStream(), "utf-8"));
-                        mBufferedWriter.write(msg + "\n");
-                        mBufferedWriter.flush();
-                        msg = null;
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private static  class StartServerRunnable implements  Runnable{
-
-        @Override
-        public void run() {
             try {
-                if (mServerSocket != null){
-                    mServerSocket = new ServerSocket(Constant.PORT);
-                    myHandler.sendEmptyMessage(1);
-                    while (true) {
-                        mSocket = mServerSocket.accept();
-                        if( mSocket !=null){
-                            myHandler.sendEmptyMessage(2);
-                        }
+                mBufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while (IS_CONNENTED) {
+
+                    String msg = mBufferedReader.readLine();
+                    if (msg != null) {
+                        String showedMsg = "server:\n" + msg + "\n";
+                        Message contentmsg = myHandler.obtainMessage();
+                        contentmsg.what = MESSAGE_SOCKET_CONNECTED;
+                        contentmsg.obj = showedMsg;
+                        myHandler.sendMessage(contentmsg);
                     }
                 }
+                mBufferedWriter.close();
+                mBufferedReader.close();
+                mSocket.close();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         }
     }
 
-
-
-    private void closeSocket() throws IOException {
-        if (mBufferedReader != null) {
-            mBufferedReader.close();
-        }
-        if (mBufferedWriter != null) {
-            mBufferedWriter.close();
-        }
-        if (mServerSocket != null) {
-            mServerSocket.close();
-        }
-    }
-
-    private static class  MyHandler extends Handler{
-        private final WeakReference<MainActivity> mActivity;
-
-        public  MyHandler(MainActivity mainActivity){
-            this.mActivity = new WeakReference<MainActivity>(mainActivity);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            MainActivity mainActivity = mActivity.get();
-            if (mainActivity != null ){
-                switch(msg.what){
-                    case  1:
-                        Log.d(TAG, "startServer");
-                        Toast.makeText(mainActivity, "startServer", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 2:
-                        Toast.makeText(mainActivity, "connect Server", Toast.LENGTH_SHORT).show();
-                        ReceiverRunnable mReceiverRunnable = new ReceiverRunnable();
-                        new Thread(mReceiverRunnable).start();
-                        SendRunnable mSendRunnable = new SendRunnable();
-                        new Thread(mSendRunnable).start();
-                        break;
-                    default:
-                        break;
-
-                }
+        /**
+         * 关闭Socket
+         *
+         * @param
+         * @return
+         * @author John
+         * @time 2017/3/2 23:47
+         */
+        private void closeSocket() throws IOException {
+            IS_CONNENTED = false;
+            if (mBufferedWriter != null) {
+                mBufferedWriter.close();
+                mBufferedWriter = null;
+            }
+            if (mBufferedReader != null) {
+                mBufferedReader.close();
+                mBufferedReader = null;
             }
 
+            if (mServerSocket != null) {
+                mServerSocket.close();
+                mServerSocket = null;
+            }
+
+
         }
+
+        private static class MyHandler extends Handler {
+            private final WeakReference<MainActivity> mActivity;
+
+            public MyHandler(MainActivity mainActivity) {
+                this.mActivity = new WeakReference<MainActivity>(mainActivity);
+            }
+
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                MainActivity mainActivity = mActivity.get();
+                if (mainActivity != null) {
+                    switch (msg.what) {
+                        case MESSAGE_RECEIVER_NEW_MSG:
+                            Log.d(TAG, "startServer");
+                            Toast.makeText(mainActivity, "startServer", Toast.LENGTH_SHORT).show();
+                            break;
+                        case MESSAGE_SOCKET_CONNECTED:
+
+                            String content = (String) msg.obj;
+                            StringBuilder sb = new StringBuilder(mainActivity.showTv.getText() + "\n");
+                            sb.append(content);
+                            mainActivity.showTv.setText(sb.toString());
+
+                            break;
+                        default:
+                            break;
+
+                    }
+                }
+
+            }
+        }
+
+    private String intToIp(int i) {
+
+        return (i & 0xFF ) + "." +
+                ((i >> 8 ) & 0xFF) + "." +
+                ((i >> 16 ) & 0xFF) + "." +
+                ( i >> 24 & 0xFF) ;
     }
 
+    private  String getLocal(){
+        //获取wifi服务
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        //判断wifi是否开启
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        String ip = intToIp(ipAddress);
+        return  ip;
+    }
 }
+
--- tcpserver/src/main/java/com/view/john/tcpserver/TcpServer.java
+++ tcpserver/src/main/java/com/view/john/tcpserver/TcpServer.java
@@ -1,6 +1,134 @@
 package com.view.john.tcpserver;
 
-public class TcpServer {
-	
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+public class TcpServer extends Service {
+
+    private boolean mIsServiceDestroyed = false;
+    private String[] mDefaultMessages = new String[]
+            {
+                    "你好啊，哈哈",
+                    "请问你叫什么名字啊？",
+                    "今天天气不错",
+                    "和你聊天很开心",
+                    "晚安"
+
+            };
+    private BufferedReader mBufferedReader;
+    private PrintWriter mBufferedWriter;
+    private ServerSocket mServerSocket;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        new Thread(new TcpServerRunnable()).start();
+        Log.d(MainActivity.TAG, "onCreate");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    private class TcpServerRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            Log.d(MainActivity.TAG, "run");
+            ServerSocket serverSocket = null;
+            try {
+                serverSocket = new ServerSocket(Constant.PORT);
+                mServerSocket = serverSocket;
+            } catch (IOException e) {
+                System.err.println("establish tcp server failed ,port:" + Constant.PORT);
+                e.printStackTrace();
+                return;
+            }
+            while (!mIsServiceDestroyed) {
+                try {
+                    final Socket client = serverSocket.accept();
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            try {
+                                responseClient(client);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        private void responseClient(Socket client) throws IOException {
+            mBufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            mBufferedWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
+            mBufferedWriter.println("welcome to here");
+            while (!mIsServiceDestroyed) {
+                String str = mBufferedReader.readLine();
+                if (str != null) {
+                    String msg = mDefaultMessages[new Random().nextInt(mDefaultMessages.length)];
+                    mBufferedWriter.println(msg);
+                }
+            }
+            mBufferedWriter.close();
+            mBufferedReader.close();
+            client.close();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(MainActivity.TAG," onDestroy");
+        mIsServiceDestroyed = true;
+//        try {
+//            closeSocket();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+    /**
+     * 关闭输出流、输入流、ServerSocket
+     * @author John
+     * @time 2017/3/3 0:13
+     * @param
+     * @return
+     *
+     */
+    private void closeSocket() throws IOException {
+        if (mBufferedWriter != null) {
+            mBufferedWriter.close();
+            mBufferedWriter = null;
+        }
+        if (mBufferedReader != null) {
+            mBufferedReader.close();
+            mBufferedReader = null;
+        }
+        if (mServerSocket != null) {
+            mServerSocket.close();
+            mServerSocket = null;
+        }
+    }
 }
